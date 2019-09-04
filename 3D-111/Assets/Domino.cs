9feae7dc2499da2847c25f401b329d9867a8c336
Isaac <isaac.robbins@veloopti.com>
2019-09-04 13:17:44 +1000

    First 3D prac

---
+++ 3D-111/Assets/Domino.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Domino : MonoBehaviour {
+	public Vector3 scale;
+	public float mass;
+	public float n;
+	public GameObject domino;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void createDomino(){
+		float off = 0;
+		for(int i = 0; i < n;i++){
+			off += (scale.y/2)*i+scale.x*i;
+			GameObject newDom = Instantiate(domino,transform.position+Vector3.right*off+Vector3.up*scale.y*i*0.5f,Quaternion.identity);
+			newDom.transform.localScale = scale * i;
+			newDom.GetComponent<Rigidbody>().mass = mass*i;
+			newDom.SetActive(true);
+		}
+	}
+
+}
---
+++ 3D-111/Assets/Editor/DominoEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Declare type of Custom Editor
+[CustomEditor(typeof(Domino))]
+public class TankControllerEditor : Editor 
+{
+ Domino _target;
+ //get the reference when starting inspection
+ void OnEnable(){
+     //target returns Object, so we cast it
+     _target = (Domino)target; 
+ }
+    // OnInspector GUI
+    public override void OnInspectorGUI()
+    {
+ DrawDefaultInspector();
+          if(GUILayout.Button("Spawn")) {
+          _target.createDomino();
+     }
+    }
+}
